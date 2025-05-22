d885ca52055b0b91371a523c2857b498edbc0da9
Hana Kong <wonbin1301@gmail.com>
2025-05-22 20:41:10 +0900

    Hover_Fueled_3D_Interactive_Image_Gallery

---
+++ Hover_Fueled_3D_Interactive_Image_Gallery/src/collection.js
@@ -0,0 +1,24 @@
+const collection = [
+    { title: "빨강", img: "./imgs/image_1.png" },
+    { title: "연두", img: "./imgs/image_2.png" },
+    { title: "파랑", img: "./imgs/image_3.png" },
+    { title: "노랑", img: "./imgs/image_4.png" },
+    { title: "분홍", img: "./imgs/image_5.png" },
+    { title: "하늘", img: "./imgs/image_6.png" },
+    { title: "진빨강", img: "./imgs/image_7.png" },
+    { title: "진초록", img: "./imgs/image_8.png" },
+    { title: "진파랑", img: "./imgs/image_9.png" },
+    { title: "올리브", img: "./imgs/image_10.png" },
+    { title: "빨강", img: "./imgs/image_11.png" },
+    { title: "연두", img: "./imgs/image_12.png" },
+    { title: "파랑", img: "./imgs/image_13.png" },
+    { title: "노랑", img: "./imgs/image_14.png" },
+    { title: "분홍", img: "./imgs/image_15.png" },
+    { title: "하늘", img: "./imgs/image_16.png" },
+    { title: "진빨강", img: "./imgs/image_17.png" },
+    { title: "진초록", img: "./imgs/image_18.png" },
+    { title: "진파랑", img: "./imgs/image_19.png" },
+    { title: "올리브", img: "./imgs/image_20.png" },
+];
+
+export default collection;
